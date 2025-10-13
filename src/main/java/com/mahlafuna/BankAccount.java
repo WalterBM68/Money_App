dba05adae4f04e537a655f7582371404a5e60039
wamanjajhb024 <wbmanjanja@gmail.com>
2025-10-13 14:50:45 +0200

    feat(BankAccount.java): The class is now having all the needed features.

--- src/main/java/com/mahlafuna/BankAccount.java
+++ src/main/java/com/mahlafuna/BankAccount.java
@@ -2,4 +2,33 @@ package com.mahlafuna;
 
 public abstract class BankAccount {
 
+    private final String accountNumber;
+    private final String accountHolderName;
+    protected double balance;
+
+    public BankAccount(String accountNumber, String accountHolderName, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = balance;
+    }
+
+    public abstract void withdraw(double amount);
+    public abstract void applyMonthlyFees();
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+
+    public void transferFunds(BankAccount targetedAccount, double amount) {
+        if (amount > 0 && balance >= amount) {
+            this.withdraw(amount);
+            targetedAccount.deposit(amount);
+        }
+    }
+
+    public String getAccountNumber() { return accountNumber; }
+    public String getAccountHolderName() { return accountHolderName; }
+    public double getBalance() { return balance; }
 }
