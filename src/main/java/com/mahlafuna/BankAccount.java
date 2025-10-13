c00ac4a8760e8f4e95cbe0ac5573a423d0c9f305
wamanjajhb024 <wamanjajhb024@student.wethinkcode.co.za>
2025-10-13 12:57:38 +0200

    first commit

--- src/main/java/com/mahlafuna/BankAccount.java
+++ src/main/java/com/mahlafuna/BankAccount.java
@@ -1,4 +1,5 @@
 package com.mahlafuna;
 
 public abstract class BankAccount {
+
 }
---
+++ src/main/java/com/mahlafuna/BankApp.java
@@ -0,0 +1,7 @@
+package com.mahlafuna;
+
+public class BankApp {
+    public static void main(String[] args) {
+
+    }
+}
