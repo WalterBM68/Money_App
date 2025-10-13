4f56dce12a42f82b6e8050d83d728aa4a504dcc5
wamanjajhb024 <wbmanjanja@gmail.com>
2025-10-13 15:09:12 +0200

    feat(SavingsAccount.java): The child class of BankAccount is implemented successfully

---
+++ src/main/java/com/mahlafuna/SavingsAccount.java
@@ -0,0 +1,27 @@
+package com.mahlafuna;
+
+public class SavingsAccount extends BankAccount {
+
+    private final double interestRate;
+    private final double minimumAmount;
+
+    public SavingsAccount(String accountHolderName, String accountNumber, double initialAmount, double interestRate) {
+        super(accountNumber, accountHolderName, initialAmount);
+        this.interestRate = interestRate;
+        this.minimumAmount = 100.0;
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        if (amount > 0 && (balance - amount) >= minimumAmount) {
+            this.balance -= amount;
+        } else  {
+            throw new IllegalArgumentException("Insufficient funds");
+        }
+    }
+
+    @Override
+    public void applyMonthlyFees() {
+        balance += balance * (interestRate / 12);
+    }
+}
