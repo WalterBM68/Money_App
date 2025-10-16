6f52c81584324e2e6999a562493b939c7292579b
Walter Manjanja <wamanjajhb024@student.wethinkcode.co.za>
2025-10-16 10:03:54 +0200

    fix(SavingsAccount.java): The class is now documented the way it supposed to

--- src/main/java/com/mahlafuna/SavingsAccount.java
+++ src/main/java/com/mahlafuna/SavingsAccount.java
@@ -1,5 +1,9 @@
 package com.mahlafuna;
 
+/**
+ * This is the Savings account that helps the users to saving their money and get the interest per month
+ * while using this account
+ */
 public class SavingsAccount extends BankAccount {
 
     private final double interestRate;
@@ -11,18 +15,24 @@ public class SavingsAccount extends BankAccount {
         this.minimumAmount = 100.0;
     }
 
+    /**
+     * Withdraw money from savings account
+     * @param amount the amount to be withdrawn from the account
+     * R7 is the amount of money will be charged everytime the money is withdrawn from ATM or Store
+     */
     @Override
     public void withdraw(double amount) {
         if (amount > 0 && (balance - amount) >= minimumAmount) {
             balance -= amount;
-            balance -= 7; //charging the user R7 if she/he withdraws money
+            balance -= 7;
         } else  {
             throw new IllegalArgumentException("Insufficient funds");
         }
     }
 
+    /**
+     * Add interest instead of charging fees per month
+     */
     @Override
-    public void applyMonthlyFees() {
-        balance += balance * (interestRate / 12);
-    }
+    public void applyMonthlyFees() { balance += balance * (interestRate / 12); }
 }
