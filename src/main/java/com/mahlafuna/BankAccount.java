6897788ae6042ed34f475531465fecba37240787
Walter Manjanja <wamanjajhb024@student.wethinkcode.co.za>
2025-10-16 10:02:37 +0200

    fix(BankAccount.java): The class is now documented the way it supposed to

--- src/main/java/com/mahlafuna/BankAccount.java
+++ src/main/java/com/mahlafuna/BankAccount.java
@@ -1,5 +1,8 @@
 package com.mahlafuna;
 
+/**
+ * Creat a bank account of your choice and be able to do all the transactions you like
+ */
 public abstract class BankAccount {
 
     private final String accountNumber;
@@ -12,15 +15,25 @@ public abstract class BankAccount {
         this.balance = initialBalance;
     }
 
+    //Allow Savings & Checking accounts to implement these methods according to their types of accounts
     public abstract void withdraw(double amount);
     public abstract void applyMonthlyFees();
 
+    /**
+     * Deposit money to the bank account
+     * @param amount the amount that is being deposited
+     */
     public void deposit(double amount) {
         if (amount > 0) {
             balance += amount;
         }
     }
 
+    /**
+     * Transfer money from one account to another legit account
+     * @param targetedAccount the account that money is being transfer to
+     * @param amount the amount of money that is transfer from one account to another
+     */
     public void transferFunds(BankAccount targetedAccount, double amount) {
         if (amount > 0 && balance >= amount) {
             this.withdraw(amount);
